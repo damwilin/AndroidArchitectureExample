0263625a3a6e162fa114d32a3a5f2f73f288cf8d
Damian <damia.wilinski@gmail.com>
2018-10-28 13:25:37 +0100

    Repository

--- app/src/main/java/com/example/wili/androidarchitectureexample/NoteDatabase.java
+++ app/src/main/java/com/example/wili/androidarchitectureexample/NoteDatabase.java
@@ -1,10 +1,13 @@
 package com.example.wili.androidarchitectureexample;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = Note.class, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -18,8 +21,33 @@ public abstract class NoteDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void>{
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db){
+            this.noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("title1", "Description 1", 1));
+            noteDao.insert(new Note("title2", "Description 2", 1));
+            noteDao.insert(new Note("title3", "Description 3", 1));
+            return null;
+        }
     }
 }
